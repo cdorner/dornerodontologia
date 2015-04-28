171ff96cb63c60c1f4dfde6a1c74f6b31d462869
Christian Dorner <christian.dorner@gmail.com>
2015-04-28 01:24:14 -0300

    Control GA (disable localhost), small link fixes

--- site/js/theme-scripts.js
+++ site/js/theme-scripts.js
@@ -14,6 +14,7 @@
 // 12. OWL SLIDER
 // 13. SCROLL TO REPLY SECTION ON POST PAGES
 // 14. CLOSE MOBILE MENU ON SELECT
+// 15. GA
 //
 // ======================================= //
 
@@ -213,3 +214,27 @@ stopOnHover: true
 });
  })(jQuery);
    // ========== END CLOSE MOBILE MENU ON SELECT ========== //
+
+// ========== 15. GA ========== //
+(function($) { "use strict";
+$(document).ready(function() {
+    if(location.href.indexOf('localhost') == -1){
+        (function (i, s, o, g, r, a, m) {
+            i['GoogleAnalyticsObject'] = r;
+            i[r] = i[r] || function () {
+                (i[r].q = i[r].q || []).push(arguments)
+            }, i[r].l = 1 * new Date();
+            a = s.createElement(o),
+                m = s.getElementsByTagName(o)[0];
+            a.async = 1;
+            a.src = g;
+            m.parentNode.insertBefore(a, m)
+        })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+
+        ga('create', 'UA-62041581-1', 'auto');
+        ga('send', 'pageview');
+    }
+});
+})(jQuery);
+
+// ========== END GA ========== //
