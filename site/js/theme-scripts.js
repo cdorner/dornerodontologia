cf4cb727b45dd8f1c5ffade2356d0516cfb78b16
Christian Dorner <christian.dorner@gmail.com>
2015-04-28 20:31:38 -0300

    Fix instagram url, adding facebook comment plugin

--- site/js/theme-scripts.js
+++ site/js/theme-scripts.js
@@ -238,3 +238,19 @@ $(document).ready(function() {
 })(jQuery);
 
 // ========== END GA ========== //
+
+
+// ========== 15. GA ========== //
+(function($) { "use strict";
+    $(document).ready(function() {
+        (function(d, s, id) {
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) return;
+            js = d.createElement(s); js.id = id;
+            js.src = "//connect.facebook.net/pt_BR/sdk.js#xfbml=1&version=v2.3&appId=634988816602696";
+            fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'))
+    });
+})(jQuery);
+
+// ========== END GA ========== //
